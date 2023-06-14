00373ecd981e3821b0979cf09c7dced90d020c63
skandertebo <alexandertebourb@gmail.com>
2023-06-14 04:06:12 +0100

    lazy loading bug fixes

--- src/routes/about/index.tsx
+++ src/routes/about/index.tsx
@@ -1,10 +1,6 @@
 import { component$, useSignal, useStylesScoped$, $ } from '@builder.io/qwik';
 import paragraphs from './paragraphs.json';
 import CSS from './index.css?inline';
-import {
-  HiChevronUpOutline,
-  HiChevronDownOutline
-} from '@qwikest/icons/heroicons';
 
 export default component$(() => {
   const currentAnimation = useSignal('fade-in');
@@ -29,7 +25,25 @@ export default component$(() => {
       <div class="flex flex-col styled-scrollbar w-full min-h-[256px]">
         {currentParagraph.value > 0 ? (
           <button class="m-0 p-0" onClick$={() => slideParagraph('up')}>
-            <HiChevronUpOutline class="w-16 h-16 shadow-lg text-slate-200 mx-auto mb-4 hover:scale-110 hover:text-slate-700 transition-all" />
+            <svg
+              class="⭐️jucv9c-1 w-16 h-16 shadow-lg text-slate-200 mx-auto mb-4 hover:scale-110 hover:text-slate-700 transition-all"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              aria-hidden="true"
+              width="1em"
+              height="1em"
+              data-qwikest-icon=""
+            >
+              <path
+                class="⭐️jucv9c-1"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M4.5 15.75l7.5-7.5 7.5 7.5"
+              ></path>
+            </svg>
           </button>
         ) : (
           <span class="h-16 mb-4"></span>
@@ -44,7 +58,25 @@ export default component$(() => {
         </p>
         {currentParagraph.value < paragraphs.length - 1 ? (
           <button class="m-0 p-0" onClick$={() => slideParagraph('down')}>
-            <HiChevronDownOutline class="w-16 h-16 shadow-lg text-slate-200 mx-auto mt-4 hover:scale-110 hover:text-slate-700 transition-all" />
+            <svg
+              class="⭐️jucv9c-1 w-16 h-16 shadow-lg text-slate-200 mx-auto mt-4 hover:scale-110 hover:text-slate-700 transition-all"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              aria-hidden="true"
+              width="1em"
+              height="1em"
+              data-qwikest-icon=""
+            >
+              <path
+                class="⭐️jucv9c-1"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+              ></path>
+            </svg>
           </button>
         ) : (
           <span class="h-16 mt-4"></span>
